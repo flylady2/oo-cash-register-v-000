bbb8008bff1b9a6d57d3bfe2f5c25b6c17bca5a1
Leslie Stevens <flylady@mail.utexas.edu>
2018-06-03 18:56:22 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount     #returns the current total
+
+  def initialize(discount = 0)     #sets an instance variable @total on initialization to zero
+    @total = 0
+    @discount = discount
+  end
+
+
+
+  def add_item(title, price, quantity = 1)      #accepts a title, price and optional quantity
+    self.total = @total + (price * quantity)   #increases the total, doesn't forget about previous total
+  end
+
+  def apply_discount                                        #applies discount if initialized with discount
+    if self.discount > 0                                    #if initialized with discount
+      discount_percentage = 1.0 - (self.discount/100.0)     #calculates percentage of full price after discount (discount_percentae)
+      @total = self.total * (discount_percentage)           #calculates total by multiplying full price by discount_percentage
+      @total                                                #returns total
+      return "After the discount, the total comes to $#{@total.to_i}."    #returns success message
+    end
+    if self.discount == 0                                   #if initialization occurs wtihout discount
+      return "There is no discount to apply."               #returns error message
+    end
+  end
+
+  def items
+    self.new_register
+
+  end
+
+end
